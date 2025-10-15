a60dc889c59330b993cdd2a1ca69cc0f46e5010a
Ankit Anand <ankit0.anand0@gmail.com>
2025-10-15 10:53:01 +0530

    [Feature] Support loading ctm as annotation

--- src/modusa/tools/ann_loader.py
+++ src/modusa/tools/ann_loader.py
@@ -6,16 +6,15 @@
 # Email: ankit0.anand0@gmail.com
 #---------------------------------
 
-from pathlib import Path
-
 def load_ann(path, clip=None):
 	"""
-	Load annotation from audatity label text file.
+	Load annotation from audatity label
+	text file and also ctm file.
 
 	Parameters
 	----------
 	path: str
-		- label text file path.
+		- label text/ctm file path.
 	clip: tuple[number, number] | number | None
 		- Incase you clipped the audio signal, this parameter will help clip the annotation.
 		- If you clip the audio, say from (10, 20), set the clip to (10, 20).
@@ -25,12 +24,23 @@ def load_ann(path, clip=None):
 	-------
 	list[tuple, ...]
 		- annotation data structure
-		- [(start, end, tag), ...]
+		- [(start, end, label), ...]
 	"""
+	from pathlib import Path
 	
 	if not isinstance(path, (str, Path)):
 		raise ValueError(f"`path` must be one of (str, Path), got {type(path)}")
 	
+	# Convert to Path object
+	path = Path(path)
+	
+	# Check if the path exists
+	if not path.exists():
+		raise FileExistsError(f"{path} does not exist")
+		
+	ann = [] # This will store the annotation
+	
+	# Clipping the annotation to match with the clipped audio
 	if clip is not None:
 		# Map clip input to the right format
 		if isinstance(clip, int or float):
@@ -40,26 +50,56 @@ def load_ann(path, clip=None):
 		else:
 			raise ValueError(f"Invalid clip type or length: {type(clip)}, len={len(clip)}")
 	
-	ann = []
-	
-	with open(str(path), "r") as f:
-		lines = [line.rstrip("\n") for line in f]
-		for line in lines:
-			start, end, tag = line.split("\t")
-			start, end = float(start), float(end)
+	if path.suffix == ".txt":
+		with open(str(path), "r") as f:
+			lines = [line.rstrip("\n") for line in f]
+			for line in lines:
+				start, end, label = line.split("\t")
+				start, end = float(start), float(end)
+				
+				# Incase user has clipped the audio signal, we adjust the annotation
+				# to match the clipped audio
+				if clip is not None:
+					offset = clip[0]
+					# Clamp annotation to clip boundaries
+					new_start = max(start, clip[0]) - offset
+					new_end   = min(end, clip[1]) - offset
+					
+					# only keep if there's still overlap
+					if new_start < new_end:
+						ann.append((new_start, new_end, label))
+				else:
+					ann.append((start, end, label))
+					
+	elif path.suffix == ".ctm":
+		with open(str(path), "r") as f:
+			content = f.read().split("\n")
 			
-			# Incase user has clipped the audio signal, we adjust the annotation
-			# to match the clipped audio
-			if clip is not None:
-				offset = clip[0]
-				# Clamp annotation to clip boundaries
-				new_start = max(start, clip[0]) - offset
-				new_end   = min(end, clip[1]) - offset
+		for c in content:
+			c = c.strip()
+			if c == "": # Handle empty line usually at the end of the ctm file
+				continue
+			elif len(c.split(" ")) != 5:
+				warnings.warn(f" '{c}' is not a standard ctm line.")
+			if len(c.split(" ")) == 5:
+				_, _, start, dur, label = c.split(" ")
+				start, dur = float(start), float(dur)
+				end = start + dur
 				
-				# only keep if there's still overlap
-				if new_start < new_end:
-					ann.append((new_start, new_end, tag))
-			else:
-				ann.append((start, end, tag))
+				# Incase user has clipped the audio signal, we adjust the annotation
+				# to match the clipped audio
+				if clip is not None:
+					offset = clip[0]
+					# Clamp annotation to clip boundaries
+					new_start = max(start, clip[0]) - offset
+					new_end   = min(end, clip[1]) - offset
+					
+					# only keep if there's still overlap
+					if new_start < new_end:
+						ann.append((new_start, new_end, label))
+				else:
+					ann.append((start, end, label))
 				
+	else:
+		raise Exception(f"Unsupported file type {path.suffix}")
 	return ann
