32fa007a80547fcc2cd8b1ac2d85c6cebfb25e77
Ankit Anand <ankit0.anand0@gmail.com>
2025-10-23 11:40:18 +0530

    release: 0.4.31

--- src/modusa/__init__.py
+++ src/modusa/__init__.py
@@ -14,4 +14,4 @@ from modusa.tools import synth_f0
 # Audio features related
 from modusa.tools import stft
 
-__version__ = "0.4.30" # This is dynamically used by the documentation, and pyproject.toml; Only need to change it here; rest gets taken care of.
+__version__ = "0.4.31" # This is dynamically used by the documentation, and pyproject.toml; Only need to change it here; rest gets taken care of.
