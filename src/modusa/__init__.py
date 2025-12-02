69a1d0b5e0739e8185039e77b1a448bd871a4a11
Ankit Anand <ankit0.anand0@gmail.com>
2025-12-02 15:24:53 +0530

    release: 3.0.0

--- src/modusa/__init__.py
+++ src/modusa/__init__.py
@@ -51,5 +51,5 @@ from ._visualizer._painter._quick_plotter import hill_plot
 #====================
 # RELEASE VERSION (MAJOR.MINOR.PATCH)
 
-__version__ = "2.2.0" # This is dynamically used by the documentation, and pyproject.toml; Only need to change it here; rest gets taken care of.
+__version__ = "3.0.0" # This is dynamically used by the documentation, and pyproject.toml; Only need to change it here; rest gets taken care of.
 #====================
