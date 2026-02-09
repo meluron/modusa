81c9a527e294e7e7113c95088f36129f60abe074
AA-MM <ankit0.anand0@gmail.com>
2026-02-09 12:27:02 +0530

    release: 6.3.7

--- src/modusa/__init__.py
+++ src/modusa/__init__.py
@@ -1,41 +1,32 @@
 # Modusa model loader
-from . import load
-
 # Modusa models
-from . import models
-
 # Media player
-from . import play
-
 # Media recorder
-from . import record
-
 # Feature computer
-from . import compute
-
 # Saver
-from . import save
-
 # Sonifier
-from . import sonify
-
 # Animator
-from . import animate
-
 # Creator. (To create figlayouts, etc)
-from . import create
-
 # Painter
-from . import paint
-
 # Setter
-from . import set
-
 # Converter
-from . import convert
+from . import (
+    animate,
+    compute,
+    convert,
+    create,
+    load,
+    models,
+    paint,
+    play,
+    record,
+    save,
+    set,
+    sonify,
+)
 
-#====================
+# ====================
 # RELEASE VERSION (MAJOR.MINOR.PATCH)
 
-__version__ = "6.2.7" # This is dynamically used by the documentation, and pyproject.toml; Only need to change it here; rest gets taken care of.
-#====================
+__version__ = "6.3.7"  # This is dynamically used by the documentation, and pyproject.toml; Only need to change it here; rest gets taken care of.
+# ====================
